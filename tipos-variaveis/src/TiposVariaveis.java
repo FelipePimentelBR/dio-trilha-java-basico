cd5b21669d17b6f4874b9b22271a678caa072f22
Felipe Pimentel Ferreira <felipe.pmt@gmail.com>
2023-01-12 00:00:39 -0300

    Exemplos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,11 +1,19 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        byte idade = 123;
-		short ano = 2021;
-		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
-		long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
-		float pi = 3.14F;
-		double salario = 1275.33;
+    public static void main(String[] args) {
+        
+		final byte idade = 123;
+		final short ano = 2021;
+		final int cep = 21070333;
+		final long cpf = 98765432109L;
+		final float pi = 3.14F;
+		final double salario = 1275.33;
+
+		System.out.println(idade);
+		System.out.println(ano);
+		System.out.println(cep);
+		System.out.println(cpf);
+		System.out.println(pi);
+		System.out.println(salario);
 
     }
 }
